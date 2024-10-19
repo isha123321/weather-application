8726dac303c622885ae85c91f9a5da9bbe15daf6
isha123321 <isha101112@hotmail.com>
2024-10-19 10:24:29 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,5 @@
+temp=int(input("enter the current weather:"))
+if temp<20:
+    print("you must wear warm clothes like sweater,muffler or socks")
+else:
+    print("you should wear light clothes like T_Shirt,shorts ")    
---
+++ a2.py
@@ -0,0 +1,30 @@
+marks=int(input("enter your marks of math:"))
+if marks<50:
+    print("you are failed in math")
+else:
+    print("you are passed in math")
+marks=int(input("enter your marks of science:"))   
+if marks<50:
+    print("you are failed in science")
+else:
+    print("you are passed in science")     
+marks=int(input("enter your marks of history:"))   
+if marks<50:
+    print("you are failed in history")
+else:
+    print("you are passed in history")     
+marks=int(input("enter your marks of geography:"))   
+if marks<50:
+    print("you are failed in geography")
+else:
+    print("you are passed in geography")     
+marks=int(input("enter your marks of urdu:"))   
+if marks<50:
+    print("you are failed in urdu")
+else:
+    print("you are passed in urdu")     
+marks=int(input("enter your marks of computer:"))   
+if marks<50:
+    print("you are failed in computer")
+else:
+    print("you are passed in computer")     
